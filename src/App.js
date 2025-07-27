0f4f24b3098af2a61dbbb6cb00883c0945eb5ba1
DanieLfotSoares <155444819+DanieLfotSoares@users.noreply.github.com>
2025-07-27 15:11:42 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+// src/App.js
+import React from 'react';
+import Game from './Components/Game';
+import './App.css';
+
+function App() {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <Game />
+            </header>
+        </div>
+    );
+}
+
+export default App;
