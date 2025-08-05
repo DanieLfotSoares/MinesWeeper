58f0a063c4cee90b28d00943165766f6569561d5
DanieLfotSoares <155444819+DanieLfotSoares@users.noreply.github.com>
2025-08-05 10:40:17 +0100

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 // src/App.js
 import React from 'react';
 import Game from './Components/Game';
+import Footer from './Components/Footer';
 import './App.css';
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
             <header className="App-header">
                 <h1>💣 Jogo das Minas</h1>
                 <Game />
+                <Footer />
             </header>
         </div>
     );
